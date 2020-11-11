5c68d9272602ab6526edd5912d759aa3988f1e40
Serach Boes <saboes@myseneca.ca>
2020-11-11 17:52:47 -0500

    added sign in

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@
 import './App.css';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Home from './Home/Home.js';
+import SignIn from './SignIn/SignIn.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      
       </header>
 
 
       <Switch>
         <Route exact path='/' render={() => <Home />} />
+        <Route exact path='/signin' render={() => <SignIn />} />
+
       </Switch>
 
     </div>
